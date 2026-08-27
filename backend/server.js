1a252c2e858730f60c13a3eb8b562fb2c6e45044
Ahmad Nasser <151929719+Ahmadnasser5@users.noreply.github.com>
2026-08-27 03:35:59 +0300

    Change server listen address to '0.0.0.0'

--- backend/server.js
+++ backend/server.js
@@ -55,6 +55,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Backend running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Backend running on port ${PORT}`);
 });
